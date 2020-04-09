79001e49b6835ae87119fe85a297d18ebc092593
Tyler Alder <tyler.b.alder@gmail.com>
2020-04-09 08:20:33 -0700

    buttons and such

--- src/composition/List.js
+++ src/composition/List.js
@@ -2,7 +2,7 @@ import React from "react";
 import Card from "./Card";
 import "./List.css";
 
-function List(props) {
+export default function List(props) {
   return (
     <section className="List">
       <header className="List-header">
@@ -10,9 +10,19 @@ function List(props) {
       </header>
       <div className="List-cards">
         {props.cards.map((card) => (
-          <Card key={card.id} title={card.title} content={card.content} />
+          <Card
+            key={card.id}
+            id={card.id}
+            title={card.title}
+            content={card.content}
+            onClickDelete={props.onClickDelete}
+          />
         ))}
-        <button type="button" className="List-add-button">
+        <button
+          type="button"
+          className="List-add-button"
+          onClick={() => props.onClickAdd(props.id)}
+        >
           + Add Random Card
         </button>
       </div>
@@ -20,4 +30,6 @@ function List(props) {
   );
 }
 
-export default List;
+List.defaultProps = {
+  onClickAdd: () => {},
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import "./index.css";
-import STORE from "./composition/store";
+import App from "./App";
 
-ReactDOM.render(<App store={STORE} />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
